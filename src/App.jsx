0d7a77855e83e1f0145da9709a2bd004bc71cbbf
Ahmed <ahmedyousif20202019@gmail.com>
2024-12-29 21:25:27 +0300

    added the imports for app jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import { CheckSession } from './services/Auth'
+import Nav from './components/Nav'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
+import ViewCategories from './pages/ViewCategories'
+import Home from './pages/Home'
