4e1de9b27d845dedbc9ca1ea9623f99c720fdc73
Ahmed <ahmedyousif20202019@gmail.com>
2024-12-30 10:35:52 +0300

    added the return for service details

--- src/categories/ServicesDetails.jsx
+++ src/categories/ServicesDetails.jsx
@@ -1,3 +1,19 @@
-const ServiceDetails = () => {}
+const ServiceDetails = () => {
+  return (
+    <div>
+      <h2>Service Details</h2>
+      <p>
+        This section provides detailed information about a particular service we
+        offer.
+      </p>
+      <ul>
+        <li>Service 1: Jobs application</li>
+        <li>Service 2: Graphic design</li>
+        <li>Service 3: Digital Marketing</li>
+      </ul>
+      <p>For more details, contact us or visit our website.</p>
+    </div>
+  )
+}
 
 export default ServiceDetails
