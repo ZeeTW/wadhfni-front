cf9b1b0df71204bf78e2e2464737ba296038a859
Ahmed <ahmedyousif20202019@gmail.com>
2024-12-30 10:48:19 +0300

    edited the app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
 import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router'
-import { Route, Routes } from 'react-router'
-import CategoryPage from './pages/Category'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink
+} from 'react-router-dom'
+import Home from './HomePage'
+import About from './About'
+import Services from './Services'
 
 const App = () => {
   return (
-    <div>
-      <main>
-        <Router>
-          <Routes>
-            <Route path="/category/:categoryId" element={<CategoryPage />} />
-          </Routes>
-        </Router>
-      </main>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                exact
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/services"
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                Services
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
