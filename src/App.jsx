550bc703e3e788db973c8ba04598f08080b54d28
Ahmed <ahmedyousif20202019@gmail.com>
2024-12-30 10:08:47 +0300

    edit app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
 import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router'
-import { CheckSession } from './services/Auth'
-import Nav from './components/Nav'
-import Register from './pages/Register'
-import SignIn from './pages/SignIn'
-import ViewCategories from './pages/ViewCategories'
-import Home from './pages/Home'
+import { Route, Routes } from 'react-router'
+import './App.css'
+import CategoryPage from './pages/Category'
 
 const App = () => {
-  const [user, setUser] = useState(null)
-
-  const handleLogOut = () => {
-    setUser(null)
-    localStorage.clear()
-  }
-  const checkToken = async () => {
-    const user = await CheckSession()
-    setUser(user)
-  }
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      checkToken()
-    }
-  }, [])
-
   return (
-    <div className="App">
-      <Nav user={user} handleLogOut={handleLogOut} />
+    <div>
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn setUser={setUser} />} />
-          <Route path="/SignUp" element={<SignUp />} />
-          <Route
-            path="/ViewCategories"
-            element={<ViewCategories user={user} />}
-          />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/category/:categoryId" element={<CategoryPage />} />
+          </Routes>
+        </Router>
       </main>
     </div>
   )
