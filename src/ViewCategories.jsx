c16a8f22e18946dd109f5e415c1a3c52f94376b3
Ahmed <ahmedyousif20202019@gmail.com>
2024-12-30 09:59:14 +0300

    added the export and functions

--- src/ViewCategories.jsx
+++ src/ViewCategories.jsx
@@ -2,3 +2,11 @@ import { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import CategoryCard from '/pages/Category'
 import axios from 'axios'
+
+const ViewCategories = () => {
+  const { genreId } = useParams()
+  const [games, setGames] = useState([])
+  const navigate = useNavigate()
+}
+
+export default ViewCategories
