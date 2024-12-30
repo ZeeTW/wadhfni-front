21140a31da02a4f151e224588ebf296c75023022
AK0275 <abdullakhalid0275@gmail.com>
2024-12-30 15:48:18 +0300

    ServiceForm

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,31 +2,34 @@ import { Link } from 'react-router-dom'
 
 const Nav = ({ handleLogOut, user }) => {
   
-if (user){
-  <div>
-      <nav>
+  return (
+    
+    <nav>
+    <div>
+      {user && (
+        <>
         <Link to="/About">About</Link>
         <Link to="/ViewCategories">View Categories</Link>
         <Link onClick={handleLogOut} to="/">Sign Out</Link>
-      </nav>
-      
-      <a href="/">
+        </>
+      )}
+    </div>
+
+    <div>
+      {user && (
+        <>
+        <a href="/auth/profile">
         <img
           src="<%= user.profilePicture %>"
           id="profile-image"
           width="60"
           height="60"
-          style="border-radius: 50%"
-        />
-      </a>
-      </div>
-
-}
-
-  return (
-  <div>
-
-  </div>
+          style="border-radius: 50%"/>
+        </a>
+        </>
+      )}
+    </div>
+    </nav>
   )
 }
 
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,22 +1,19 @@
 import { Link } from 'react-router-dom'
 
 
-const NavLinks = ({user}) => {
-
-    if (user) {
-        <div>
-            <Link to="/Services">Graphic designer</Link>
-            <Link to="/Services">Digital Marketing</Link>
-            <Link to="/Services">Translation & copywriting</Link>
-            <Link to="/Services">Consultation</Link>
-        </div>
-
-    }
+const NavLinks = ({ user }) => {
 
 
     return (
     <div>
-
+        {user && (
+            <>
+            <Link to="/Services">Graphic designer</Link>
+            <Link to="/Services">Digital Marketing</Link>
+            <Link to="/Services">Translation & copywriting</Link>
+            <Link to="/Services">Consultation</Link>
+            </>
+        )}
     </div>
     )
 }
