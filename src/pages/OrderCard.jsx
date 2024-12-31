0d1f1da933ddd4b5dd96b8d676b6de17d09d749e
Ahmed <ahmedyousif20202019@gmail.com>
2024-12-31 13:16:47 +0300

    added handle order click for the order file

--- src/pages/OrderCard.jsx
+++ src/pages/OrderCard.jsx
@@ -1,16 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+
 const OrderCard = ({ onClick, image, name, rating }) => {
+  const navigate = useNavigate()
+
+  const handleOrderClick = () => {
+    navigate('/OrderDetails')
+  }
+
   return (
     <div className="card order-card" onClick={onClick}>
       <div className="img-wrapper">
         <img src={image} alt={name} />
-        <button id="Order-List" onClick={() => navigate('/OrderDetails')}>
+        <button id="Order-List" onClick={handleOrderClick}>
           Order Details:
         </button>
       </div>
 
       <div className="info-wrapper">
         <h3>{name}</h3>
-        <p>Rating:{rating}</p>
+        <p>Rating: {rating}</p>
       </div>
     </div>
   )
