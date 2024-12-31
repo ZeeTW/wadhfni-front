c455e785a3fa03d61acaf21c544ef90a04bccf4c
AK0275 <abdullakhalid0275@gmail.com>
2024-12-31 09:53:48 +0300

    Profile

---
+++ src/pages/MyOrders.jsx
@@ -0,0 +1,13 @@
+import OrderCard from "../components/OrderCard"
+
+const MyOrders = () => {
+
+
+return (
+    <div>
+    
+    </div>
+    )
+}
+
+export default MyOrders
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Profile = ({props}) => {
 
-    <div class="profile-container">
+<div class="profile-container">
     <h1 id="h1-profile-user">User Profile</h1>
     <img
     src= {user.profilePicture || '/default-profile.png' } 
@@ -10,23 +10,29 @@ const Profile = ({props}) => {
 />
 </div>
 
-    return (
-    <div>
+return (
+<div>
     <p><strong>Name:</strong>{props.user}</p>
     <p><strong>Bio:</strong>{props.Bio}</p>
 
-
-<div className="profile-button">
+<div className="profile-btn-Fl">
+    {user.Freelancer && (
+    <>
         <Link to='/'><button>Sign Out</button></Link>
         <Link to='/ServiceForm'><button>Add a Service</button></Link>
+    </>
+    )}
 </div>
 
-
-
+<div className="profile-btn-em">
+    {user.Employer && (
+    <>
+        <Link to='/'><button>Sign Out</button></Link>
+        <Link to='/MyOrders'><button>View My Orders</button></Link>
+    </>
+    )}
     </div>
-
-    
-
+    </div>
     )
 }
 
