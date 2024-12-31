ecc5c2c071a63ca027a05933c5d40fb7c8f9da31
AK0275 <abdullakhalid0275@gmail.com>
2024-12-31 10:18:38 +0300

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,10 @@ const About = () => {
 
 return (
     <div>
-    <h1>About Page</h1>
-    <p className="about-paragraph">About Wa'6fni</p>
+    <h1>About Wa'6fni</h1>
+    <p className="about-paragraph">Welcome to Wa'6fni, your go-to platform for hiring top-notch freelancers for any project. Whether you're an entrepreneur, a business, or an individual, Wa'6fni is here to bridge the gap between you and the talented professionals you need to bring your ideas to life.</p>
+    <p className="about-paragraph">At Wa'6fni, we make it simple to find and hire expert freelancers across a wide range of services, including Graphic designer, Digital Marketing, Translation & copywriting, Consultation, and much more. Our platform is designed with you in mind, offering an intuitive interface that lets you post your job, review candidates, and collaborate with ease.</p>
+    <p className="about-paragraph">Join a growing community of employers and freelancers who trust Wa'6fni to deliver high-quality results on time and within budget. Whether you're a freelancer looking for your next gig or an employer seeking skilled professionals, Wa'6fni makes the connection smooth, reliable, and efficient.</p>
 </div>
 )
 }
