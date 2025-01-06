d76f2e41b5b0ecce0b6f1ae2f47ddb61d3eb031e
mahalabsi <mahatalabsi@gmail.com>
2025-01-06 11:14:29 +0300

    .

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const About = () => {
-
-return (
+  return (
     <div>
-    <h1>About Page</h1>
-    <p className="about-paragraph">About Wa'6fni</p>
-</div>
-)
+      <h1>About Page</h1>
+
+      <p className="about-paragraph">
+        Welcome to the 5aleji app for services. don't worry in time it will look
+        more cooler{' '}
+      </p>
+    </div>
+  )
 }
 
 export default About
